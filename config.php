a212c3d3c57d5fe8f759328abd1838a08d98ddbb
Cameron Smith <31256547+smithcg2@users.noreply.github.com>
2019-03-13 12:34:00 -0400

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define('DB_SERVER','localhost');
-	define('DB_USERNAME','root');
-	define('DB_PASSWORD','Smithcg4792');
-	define('DB_DATABASE','Dappr');
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
